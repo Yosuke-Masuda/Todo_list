6450fc12bffb893e64c99d8bc6059cc8e1a37928
Yosuke-Masuda <y.m090918@gmail.com>
2024-09-29 02:55:23 +0900

    [Update] index.js localStorage

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
+// localStorageからデータを取得　JSON.parseを使用することで元の配列として扱うことができる
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+// liタグが空ではない場合の分岐
+if(todos){
+  todos.forEach(todo => {
+    add(todo);
+  })
+}
+
 // functionで関数宣言して、関数名、引数にとるものを記述。
 // 例　function double(num) {
 //      return num * 2;
@@ -20,12 +30,16 @@ form.addEventListener("submit", function(event){
   add();
 })
 
-function add() {
+function add(todo) {
 
   // ifで空の時はfalse。処理が実行されない。
   // 今回は真偽値を暗黙的型変換を使用（使用しないときの例：todoText.length > 0）
 
   let todoText = input.value;
+
+  if (todo) {
+    todoText = todo;
+  }
   if (todoText) {
     const li = document.createElement("li");
     li.innerText = todoText;
@@ -34,5 +48,22 @@ function add() {
     ul.appendChild(li);
     // 投稿後フォームを空にする
     input.value = "";
+    saveData();
   }
+}
+
+// localStorage：ブラウザーにデータを保存しておく仕組み(永続的に保存)
+// localStorageは文字列形式で格納されるので保存前にJSON形式を使用し文字列として扱える
+// データの保存
+// localStorage.setItem("キー", '値');
+// データの取得
+// localStorage.getItem('キー')
+
+function saveData() {
+  const lists = document.querySelectorAll("li");
+  let todos = [];
+  lists.forEach(list => {
+    todos.push(list.innerText);
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
