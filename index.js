a5b1f51ae3fe2e1ddb28434e0b033bfc3b1e542a
Yosuke-Masuda <y.m090918@gmail.com>
2024-09-29 04:15:54 +0900

    [Add] index.js listsの完了状態、削除機能

--- index.js
+++ index.js
@@ -38,12 +38,33 @@ function add(todo) {
   let todoText = input.value;
 
   if (todo) {
-    todoText = todo;
+    todoText = todo.text;
   }
+  
   if (todoText) {
     const li = document.createElement("li");
     li.innerText = todoText;
     li.classList.add("list-group-item");
+
+    if (todo && todo.completed) {
+      li.classList.add("text-decoration-line-through");
+    }
+
+    // 右クリック(contextmenu)というイベントで削除できるように実装
+    li.addEventListener("contextmenu", function
+    (event){
+      event.preventDefault();
+      li.remove();
+
+      // localStorageに反映
+      saveData();
+    });
+
+    li.addEventListener("click", function(){
+      li.classList.toggle
+      ("text-decoration-line-through");
+      saveData();
+    });
     // ulタグの子供としてliタグ追加
     ul.appendChild(li);
     // 投稿後フォームを空にする
@@ -60,10 +81,21 @@ function add(todo) {
 // localStorage.getItem('キー')
 
 function saveData() {
+
+  // 全ての"li"を配列で取得
   const lists = document.querySelectorAll("li");
   let todos = [];
+
+  // forEach:配列の全要素に処理
   lists.forEach(list => {
-    todos.push(list.innerText);
+    let todo = {
+      text: list.innerText,
+      completed: list.classList.contains
+      ("text-decoration-line-through")
+    };
+
+    // 配列に要素を追加
+    todos.push(todo);
   });
   localStorage.setItem("todos", JSON.stringify(todos));
 }
